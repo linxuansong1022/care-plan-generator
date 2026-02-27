5349a4af69d266b88467da8a2e5779e06ba434f8
linxuansong1022 <linxuansong1022@gmail.com>
2026-02-27 23:08:31 +0100

    Init terraform (excluding binaries)

---
+++ careplan-terraform/main.tf
@@ -0,0 +1,14 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"  
+}
